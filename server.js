019c9f883e682cbd7c1f74cb6760107558c51e65
Nafiz Alam <nafiz446760@gmail.com>
2023-06-01 20:10:25 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/UserRoutes");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb+srv://nafiz446760:nafiz@project-2.qldtu2v.mongodb.net/?retryWrites=true&w=majority", {
+    dbName:"netflix"
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/user", userRoutes);
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
